7e3cf1ba1b558ddcc7cd6c129592952c3275bee2
Akshay Sharma <akshaysharmaay@gmail.com>
2023-06-26 10:52:14 +0530

    Created first data type: User

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,7 @@
+// TODO: To use "nodemon" package so that it hot reloads
 import { config, createSchema } from "@keystone-next/keystone/schema";
 import "dotenv/config";
+import { User } from "./schemas/User";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits";
@@ -23,6 +25,7 @@ export default config({
   },
   lists: createSchema({
     // Schema items  goes here
+    User,
   }),
   ui: {
     // TODO: change this for roles
